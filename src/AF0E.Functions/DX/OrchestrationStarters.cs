46df69092d746ede167fd297d6062f84358c9e73
Alex <aleksey_grachev@yahoo.com>
2024-02-10 13:42:20 -0700

    DS scraper. Closes #2 (#16)

---
+++ src/AF0E.Functions/DX/OrchestrationStarters.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.DurableTask.Client;
+using AF0E.Functions.DX.Orchestrators;
+
+#pragma warning disable CA1062 // null public methods arguments
+
+namespace AF0E.Functions.DX;
+
+public sealed class OrchestrationStarters
+{
+    /*
+    private readonly ILogger<OrchestrationStarters> _logger;
+    
+    public OrchestrationStarters(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<OrchestrationStarters>();
+    }
+    */
+    
+#pragma warning disable IDE0060
+    // ReSharper disable once UnusedParameter.Global
+    //[Function(nameof(DxScraperTimerStarter))]
+    public async Task DxScraperTimerStarter([TimerTrigger("0 */1 * * * *")] TimerInfo timerInfo, [DurableClient] DurableTaskClient starter) // 0 0 0 * * * - at 12am
+#pragma warning restore IDE0060
+    {
+        await starter.ScheduleNewOrchestrationInstanceAsync(DxScraperOrchestrator.OrchestratorName);
+    }
+        
+    [Function(nameof(DxScraperHttpStarter))]
+    public async Task<HttpResponseData> DxScraperHttpStarter([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, [DurableClient] DurableTaskClient starter) 
+    {
+        var instanceId = await starter.ScheduleNewOrchestrationInstanceAsync(DxScraperOrchestrator.OrchestratorName);
+        return await starter.CreateCheckStatusResponseAsync(req, instanceId);
+    }        
+}
+
+#region TimerInfo
+// ReSharper disable UnusedMember.Global
+// ReSharper disable ClassNeverInstantiated.Global
+public class TimerInfo
+{
+    public TimerSchedule? Schedule { get; set; }
+    public ScheduleStatus? ScheduleStatus { get; set; }
+    public bool IsPastDue { get; set; }
+}
+
+public class TimerSchedule
+{
+    public bool AdjustForDst { get; set; }
+}
+
+public class ScheduleStatus
+{
+    public DateTime Last { get; set; }
+    public DateTime Next { get; set; }
+    public DateTime LastUpdated { get; set; }
+}
+#endregion
---
+++ src/AF0E.Functions/DX/Orchestrators/DxScraperOrchestrator.cs
@@ -0,0 +1,43 @@
+﻿using AF0E.Functions.DX.Activities;
+using AF0E.Functions.DX.Infrastructure;
+#pragma warning disable CA1062
+
+namespace AF0E.Functions.DX.Orchestrators;
+
+public sealed class DxScraperOrchestrator
+{
+    public const string OrchestratorName = nameof(DxScraperOrchestrator);
+
+    [Function(OrchestratorName)]
+    public static async Task Orchestrator([OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        var log = context.CreateReplaySafeLogger(typeof(DxScraperOrchestrator));
+
+        var activities = new List<Task<ScrapeActivityResult>>
+        {
+            context.CallActivityAsync<ScrapeActivityResult>(DxInfoDotNetActivity.ActivityName),
+            context.CallActivityAsync<ScrapeActivityResult>(DxMapsDotComActivity.ActivityName),
+            context.CallActivityAsync<ScrapeActivityResult>(Va3RjActivity.ActivityName),
+        };
+
+        await Task.WhenAll(activities);
+
+        var dxData = activities
+            .Where(x => x is { IsCompletedSuccessfully: true, Result.IsSuccess: true })
+            .Select(x => x.Result.Result).SelectMany(x => x!).ToList();
+
+        if (dxData.Count == 0)
+        {
+            log.LogNoDx();
+            return;
+        }
+
+        var dt = context.CurrentUtcDateTime;
+        var minDate = new DateTime(dt.Year, dt.Month, dt.Day);
+        var maxReceivedDate = dxData.Max(x => x.EndDate);
+
+        var savedData = await context.CallActivityAsync<List<DxInfo>>(LoadSavedDataActivity.ActivityName, new Tuple<DateTime, DateTime>(minDate, maxReceivedDate));
+
+        await context.CallActivityAsync(MergeAndSaveActivity.ActivityName, new Tuple<List<DxInfo>, List<DxInfo>, DateTime>(savedData, dxData, minDate));
+    }
+}
