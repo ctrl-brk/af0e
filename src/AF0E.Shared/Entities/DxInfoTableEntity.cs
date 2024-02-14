6e8bebb8dac45a354b24895b4e27f2a298d45fd1
Alex <aleksey_grachev@yahoo.com>
2024-02-13 21:00:10 -0700

    dotnet8 (#18)

---
+++ src/AF0E.Shared/Entities/DxInfoTableEntity.cs
@@ -0,0 +1,25 @@
+﻿using Azure;
+using Azure.Data.Tables;
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+// ReSharper disable MemberCanBeProtected.Global
+
+namespace AF0E.Shared.Entities;
+
+public class DxInfoTableEntity : ITableEntity
+{
+    public string? Name { get; set; }
+    public string? DXCC { get; set; }
+    public string? IOTA { get; set; }
+    public string CallSign { get; set; } = null!; //set is for json only
+    public DateTime BeginDate { get; set; }
+    public bool BeginDateSet { get; set; }
+    public DateTime EndDate { get; set; }
+    public bool EndDateSet { get; set; }
+    public string? Links { get; set; } //set is for json only
+    public string? Description { get; set; }
+
+    public string PartitionKey { get; set; } = null!;
+    public string RowKey { get; set; } = null!;
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+}
---
+++ src/AF0E.WebApi/DX/Program.cs
@@ -0,0 +1,24 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/active", () =>
+{
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
