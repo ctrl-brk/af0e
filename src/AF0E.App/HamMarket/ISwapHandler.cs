3ffdacc1f81bc20828e39e6b7b5ddf5983ffc433
Alex <aleksey_grachev@yahoo.com>
2024-03-13 19:21:24 -0600

    added ham market

---
+++ src/AF0E.App/HamMarket/ISwapHandler.cs
@@ -0,0 +1,8 @@
+﻿// ReSharper disable once CheckNamespace
+namespace HamMarket;
+
+public interface ISwapHandler
+{
+    Task<ScanResult> ProcessKeywordsAsync(HttpClient client, CookieContainer cookies, CancellationToken token);
+    Task<IEnumerable<ScanResult>> ProcessCategoriesAsync(HttpClient httpClient, CookieContainer cookies, CancellationToken token);
+}
---
+++ src/AF0E.App/HamMarket/Utils.cs
@@ -0,0 +1,90 @@
+﻿namespace HamMarket;
+
+public static class Utils
+{
+    internal static string GetValue(string src, string startToken, string endToken, ref int index, bool req = true)
+    {
+        var start = src.IndexOf(startToken, index, StringComparison.OrdinalIgnoreCase);
+        if (start < 0)
+        {
+            if (!req) return null;
+            throw new ApplicationException("Invalid response format");
+        }
+        var end = src.IndexOf(endToken, start + startToken.Length, StringComparison.OrdinalIgnoreCase);
+        if (end < 0) throw new ApplicationException("Invalid response format");
+
+        index = end + endToken.Length;
+
+        var res = src.Substring(start + startToken.Length, end - (start + startToken.Length));
+
+        return res;
+    }
+
+    internal static string HighlightPrices(string text)
+    {
+        var prevInd = 0;
+        var ind = text.IndexOf('$');
+
+        if (ind < 0) return text;
+
+        var result = new StringBuilder();
+
+        while (ind >= 0)
+        {
+            result.Append(text.AsSpan(prevInd, ind - prevInd));
+            result.Append("<span class='price'>$");
+            ind++;
+            var cnt = 0;
+            for (; cnt < 15 && ind < text.Length && (char.IsNumber(text[ind]) || new[] { ' ', ',', '.' }.Contains(text[ind])); cnt++)
+            {
+                result.Append(text[ind++]);
+            }
+            result.Append("</span>");
+
+            prevInd = ind;
+
+            if (ind + cnt >= text.Length) break;
+
+            ind = text.IndexOf('$', ind + cnt);
+        }
+
+        if (ind < text.Length)
+            result.Append(text.AsSpan(prevInd));
+
+        return result.ToString();
+    }
+
+    internal static string GetPrice(Post post)
+    {
+        var value = "";
+        var cnt = 0;
+
+        var ind = post.Description.LastIndexOf('$');
+        if (ind < 0) return null;
+        ind++;
+        while (cnt < 15 && ind < post.Description.Length && (char.IsNumber(post.Description[ind]) || new[] { ' ', ',', '.' }.Contains(post.Description[ind])))
+        {
+            value += post.Description[ind++];
+            cnt++;
+        }
+
+        return value.Length > 0 ? value.Trim() : null;
+    }
+
+    internal static async Task GetImage(HttpClient httpClient, AppSettings settings, Post post, string format, Cache cache)
+    {
+        if (!post.HasImage || string.IsNullOrEmpty(cache?.ImageFolder)) return;
+
+        var fName = $"{settings.ResourceFolder}/{cache.ImageFolder}/{post.Id}.jpg";
+        if (File.Exists(fName)) return;
+
+        var uri = new Uri(string.Format(format, post.Id));
+
+        using var res = await httpClient.GetAsync(uri);
+        res.EnsureSuccessStatusCode();
+
+        var data = await res.Content.ReadAsByteArrayAsync();
+        Directory.CreateDirectory(cache.ImageFolder);
+        await File.WriteAllBytesAsync(fName, data);
+    }
+}
