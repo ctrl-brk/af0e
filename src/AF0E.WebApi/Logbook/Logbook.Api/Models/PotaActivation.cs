382c6a23193f4fb66f3b781de8140f9487859ba7
Alex <aleksey_grachev@yahoo.com>
2024-12-12 17:16:06 -0700

    merge dev (#50)
    * Dxscraper date va3rj date fix (#43)
    
    * date parsing
    
    * packages upgrade
    
    * Local api (#45)
    
    * work in progress
    
    * UI and API
    
    * gridtracker api (#47)
    
    * rename (#49)

---
+++ src/AF0E.WebApi/Logbook/Logbook.Api/Models/PotaActivation.cs
@@ -0,0 +1,23 @@
+﻿namespace Log.Api.Models;
+
+internal sealed class PotaActivation
+{
+    public int ActivationId { get; init; }
+    public int ParkId { get; init; }
+    public string Grid { get; init; } = null!;
+    public string? City { get; init; } = null!;
+    public string County { get; init; } = null!;
+    public string State { get; init; } = null!;
+    public DateTime StartDate { get; init; }
+    public DateTime? EndDate { get; init; }
+    public DateTime? LogSubmittedDate { get; init; }
+    // ReSharper disable once EntityFramework.ModelValidation.UnlimitedStringLength
+    public string? SiteComments { get; init; }
+    public string? Comments { get; init; }
+    public decimal? Lat { get; init; }
+    public decimal? Long { get; init; }
+
+    public PotaPark Park { get; init; } = null!;
+    // ReSharper disable once CollectionNeverUpdated.Global
+    public ICollection<PotaContact> PotaContacts { get; init; } = [];
+}
