900e0cf0a81bbc67c2f85cc501c2b1bbd6186fd5
Alex <aleksey_grachev@yahoo.com>
2025-02-19 11:54:51 -0700

    eham.net disabled for now (#68)

--- src/AF0E.DB/Scripts/Update DXCC countries.sql
+++ src/AF0E.DB/Scripts/Update DXCC countries.sql
@@ -1,4 +1,11 @@
---select COL_COUNTRY, * from TABLE_HRD_CONTACTS_V01 where COL_COUNTRY is not null and COL_COUNTRY <> '[none]' and COL_COUNTRY not in (select EntityName from Dxcc)
+/*
+select distinct l.COL_COUNTRY
+               ,l.COL_CALL
+  from TABLE_HRD_CONTACTS_V01 l where l.COL_COUNTRY is not null and l.COL_COUNTRY <> '[none]' and l.COL_COUNTRY not in (select EntityName from Dxcc) and not exists (select 1 from Dxcc where AltNames like '%|' + l.COL_COUNTRY + '|%') order by l.COL_COUNTRY
+*/
+/*
+select l.COL_COUNTRY, l.*  from TABLE_HRD_CONTACTS_V01 l where l.COL_COUNTRY is not null and l.COL_COUNTRY <> '[none]' and l.COL_COUNTRY not in (select EntityName from Dxcc)
+*/
 --begin tran
 update TABLE_HRD_CONTACTS_V01
    set COL_COUNTRY = d.EntityName
