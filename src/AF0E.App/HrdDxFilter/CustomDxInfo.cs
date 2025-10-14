b70e4dec7e5548b71a5ef1a703a8282f8a35a226
Alex <aleksey_grachev@yahoo.com>
2025-10-13 18:29:13 -0600

    custom dx file processing

---
+++ src/AF0E.App/HrdDxFilter/CustomDxInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace HrdDxFilter;
+
+public class CustomDxInfo
+{
+    public DateTime BeginDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string CallSigns { get; set; } = null!;
+    public string? Comment { get; set; }
+}
